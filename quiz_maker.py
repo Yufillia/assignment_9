fc0408181fca19a5ede607d1d8c0484cc1749d67
Yufillia <yuufiii29@gmail.com>
2025-04-15 07:01:29 +0800

    Created save_questions() function to get and validate input. Also added an if statement whenever the user inputs wrongly.

--- quiz_maker.py
+++ quiz_maker.py
@@ -2,3 +2,15 @@ import tkinter as tk
 from tkinter import messagebox
 
 questions = []
+
+def save_question():
+    question = entry_question.get()
+    a = entry_a.get()
+    b = entry_b.get()
+    c = entry_c.get()
+    d = entry_d.get()
+    answer = answer_var.get()
+
+    if not question or not all([a, b, c, d]) or answer not in ['a', 'b', 'c', 'd']:
+        messagebox.showerror("Error", "Please fill all fields correctly.")
+        return
