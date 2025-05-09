e7d8e4842328d041c873e0a76db60c3ac1a06e8a
Yufillia <yuufiii29@gmail.com>
2025-05-09 18:27:12 +0800

    Added function ro load quiz questions from text file into a list of dicitonaries

--- quiz_program.py
+++ quiz_program.py
@@ -1,3 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+
+def loaded_questions(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        content = f.read().strip().split("---\n")
+    
+    questions = []
+    for block in content:
+        lines = block.strip().split("\n")
+        if len(lines) < 6:
+            continue
+        
+        q = {
+            "question": lines[0].replace("Question: ", ""),
+            "a": lines[1].replace("a) ", ""),
+            "b": lines[2].replace("b) ", ""),
+            "c": lines[3].replace("c) ", ""),
+            "d": lines[4].replace("d) ", ""),
+            "answer": lines[5].replace("Answer: ", "").strip().lower()
+            
+        }
+        questions.append(q)
+    return questions
